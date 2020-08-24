64a67e8bd8bb62bedc90b0d71efc1f78e890aa47
miura_kotaro <miura_kotaro@interprism.co.jp>
2020-08-24 15:21:13 +0900

    first

---
+++ src/main/java/jr/pricing/jrpricing/JrPricingApplication.java
@@ -0,0 +1,13 @@
+package jr.pricing.jrpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrPricingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jr/pricing/jrpricing/JrPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package jr.pricing.jrpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrPricingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
