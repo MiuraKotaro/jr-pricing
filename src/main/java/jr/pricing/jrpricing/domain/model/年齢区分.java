4a14946aff34943e0589ed9cb0423b754fcd6847
miura_kotaro <miura_kotaro@interprism.co.jp>
2020-08-27 17:31:24 +0900

    年齢区分割引計算　追加

--- src/main/java/jr/pricing/jrpricing/domain/model/年齢区分.java
+++ src/main/java/jr/pricing/jrpricing/domain/model/年齢区分.java
@@ -1,14 +1,17 @@
 package jr.pricing.jrpricing.domain.model;
 
 import jr.pricing.jrpricing.domain.model.fare.割引率;
+import jr.pricing.jrpricing.domain.model.fare.料金;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public enum 年齢区分 {
     おとな(new 割引率(0)),
     こども(new 割引率(50));
 
-    割引率 運賃割引率;
+    private final 割引率 _割引率;
+
+    public 料金 割引計算(料金 割引前) {
+        return 割引前.割引(_割引率);
+    }
 }
--- src/main/java/jr/pricing/jrpricing/domain/model/片道往復.java
+++ src/main/java/jr/pricing/jrpricing/domain/model/片道往復.java
@@ -6,8 +6,8 @@ import jr.pricing.jrpricing.domain.model.fare.料金;
 public enum 片道往復 {
     片道 {
         @Override
-        public 料金 往復割引計算(料金 一人分片道運賃, 距離 _距離) {
-            return 一人分片道運賃;
+        public 料金 往復割引計算(料金 一人分片道割引前運賃, 距離 _距離) {
+            return 一人分片道割引前運賃;
         }
     },
     往復 {
@@ -15,10 +15,10 @@ public enum 片道往復 {
         private final 距離 往復割引最短距離 = new 距離(600);
 
         @Override
-        public 料金 往復割引計算(料金 一人分片道運賃, 距離 _距離) {
-            return _距離.大なり(往復割引最短距離) ? 一人分片道運賃.割引(運賃割引率) : 一人分片道運賃;
+        public 料金 往復割引計算(料金 一人分片道割引前運賃, 距離 _距離) {
+            return _距離.大なり(往復割引最短距離) ? 一人分片道割引前運賃.割引(運賃割引率) : 一人分片道割引前運賃;
         }
     };
 
-    public abstract 料金 往復割引計算(料金 一人分片道運賃, 距離 _距離);
+    public abstract 料金 往復割引計算(料金 一人分片道割引前運賃, 距離 _距離);
 }
