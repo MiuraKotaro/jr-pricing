cf473c55b0c7917214d367a270434eb6d3887195
miura_kotaro <miura_kotaro@interprism.co.jp>
2020-08-27 15:29:19 +0900

    get無料人数() 追加

--- src/main/java/jr/pricing/jrpricing/domain/model/人数.java
+++ src/main/java/jr/pricing/jrpricing/domain/model/人数.java
@@ -16,13 +16,17 @@ public class 人数 {
     public 人数 足す(人数 足す人数) {
         return new 人数(_人数 + 足す人数._人数);
     }
+
     public 人数 引く(人数 引く人数) {
         return new 人数(_人数 - 引く人数._人数);
     }
+
     public boolean 大なりイコール(人数 比較人数) {
         return _人数 >= 比較人数._人数;
     }
-    public 人数 plus49div50() {
-        return new 人数((_人数 + 49) / 50);
+
+    public 人数 get無料人数() {
+        final 人数 無料化最少人数 = new 人数(31);
+        return this.大なりイコール(無料化最少人数) ? new 人数((_人数 + 49) / 50) : new 人数(0);
     }
 }
