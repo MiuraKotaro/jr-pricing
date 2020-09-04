332fdc1db4032957852d0375793d570b3e1c21fc
miura_kotaro <miura_kotaro@interprism.co.jp>
2020-09-04 10:58:41 +0900

    団体割引ドメインサービス

--- src/main/java/jr/pricing/jrpricing/application/service/料金計算サービス.java
+++ src/main/java/jr/pricing/jrpricing/application/service/料金計算サービス.java
@@ -1,8 +1,8 @@
 package jr.pricing.jrpricing.application.service;
 
-import jr.pricing.jrpricing.domain.model.fare.割引率;
 import jr.pricing.jrpricing.domain.model.fare.料金;
 import jr.pricing.jrpricing.domain.model.*;
+import jr.pricing.jrpricing.domain.service.団体割引ドメインサービス;
 
 public class 料金計算サービス {
     public static 料金 料金計算(駅 降車駅, 利用日 _利用日, 片道往復 _片道往復, 人数 大人人数, 人数 子供人数, 特急利用 _特急利用) {
@@ -37,24 +37,7 @@ public class 料金計算サービス {
 
     private static 料金 運賃割引計算(料金 一人分片道運賃, 距離 _距離, 片道往復 _片道往復, 利用日 _利用日, 人数 大人人数, 人数 子供人数) {
         一人分片道運賃 = _片道往復.往復割引計算(一人分片道運賃, _距離);
-        一人分片道運賃 = 団体時期割引計算(一人分片道運賃, _利用日, 大人人数, 子供人数);
+        一人分片道運賃 = 団体割引ドメインサービス.団体時期割引計算(一人分片道運賃, _利用日, 大人人数, 子供人数);
         return 一人分片道運賃;
-    }
-
-    // TODO: 2020/09/01 これはドメイン知識があるからドメインサービスにしたい
-    private static 料金 団体時期割引計算(料金 一人分片道運賃, 利用日 _利用日, 人数 大人人数, 人数 子供人数) {
-        final 人数 団体割引最少人数 = new 人数(8);
-        final 割引率 団体年末年始割引率 = new 割引率(10);
-        final 割引率 団体割引率 = new 割引率(15);
-        final 人数 全体人数 = 大人人数.足す(子供人数);
-
-        if (団体割引最少人数.compareTo(全体人数) > 0) {
-            return 一人分片道運賃;
-        }
-        if (_利用日.isAfter(12, 20) || _利用日.isBefore(1, 11)) {
-            return 一人分片道運賃.割引(団体年末年始割引率);
-        } else {
-            return 一人分片道運賃.割引(団体割引率);
-        }
     }
 }
